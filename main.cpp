7698949400e17fade1261f511add0a5fe0d68071
Nate-OBrien <159456288+Nate-OBrien@users.noreply.github.com>
2024-09-24 10:01:49 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int userGuess;
+int main(){
+    int anshBench = 515;
+
+    while (true){
+        cout << "\nHow much can ansh bench? ";
+        cin >> userGuess;
+        if (userGuess == 515){
+            cout << "CORRECT!!!!" << endl;
+            break;
+        } else if (userGuess == 120){
+            cout << "...uhhh this is for sure not right... definitely." << endl;
+            break;
+        } else if (userGuess < 515){
+            cout << "Wrong! Ansh is so much stronger than that." << endl;
+        } else if (userGuess > 515){
+            cout << "WRONG! Ansh is weaker than that." << endl;
+        }
+        userGuess = 0;
+    }
+
+    return 0;
+}
